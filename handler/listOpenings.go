b2f5e7953ba828c591744d14bf8ce1c77e3c67e5
Gabriel barreto <gabriel.barreto123@gmail.com>
2024-11-01 17:29:20 -0300

    feat: add del/list/up

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -3,11 +3,17 @@ package handler
 import (
 	"net/http"
 
+	"github.com/gabrielbarretoo/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET openings!",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error fetching openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
 }
--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -1,13 +1,64 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/gabrielbarretoo/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "PUT opening!",
-	})
+	request := UpdateOpeningRequest{}
+	ctx.BindJSON(&request)
+
+	logger.Info(request)
+
+	err := request.Validate()
+	if err != nil {
+		logger.Errorf("error validating opening: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	if err := db.Save(&opening).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+
+	sendSuccess(ctx, "update-opening", opening)
+
 }
